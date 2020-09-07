ff7ce4c5aa599bcdb8e56d297b254b0cd9ebefad
Ranjani <vranju95@gmail.com>
2020-09-07 13:29:41 -0500

    handle sample JSON file and add http response to the request

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const todo = {
+    "Upcoming-task": "Do this",
+    "Ongoing-task": "Do that",
+    "copleted-task": "This and that"
+};
+
+const getJsonString = (obj) => JSON.stringify(obj, null, '  ');
 
 const server = http.createServer(handler);
 
 function handler(request, response) {
-    response.writeHead(200, {
-        "Content-Type": "application/json"
-    });
-    response.write("Done");
+    if (request.url === '/todo') {
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.write(getJsonString(todo));
+    } else {
+        response.writeHead(404);
+        response.write('Not Found');
+    }
     response.end();
 }
 server.listen(8080);
