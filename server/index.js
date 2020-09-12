0cc804c87eceef5e4ad8e8acab71f7612d3fef06
Ranjani Gopi <vranju95@gmail.com>
2020-09-12 13:38:02 -0500

    add sample routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,23 @@ const http = require('http');
 const file = require('./file');
 const router = require('./router');
 
+router.get('/birthday', function(request, response) {
+    const now = new Date();
+    if (now.getMonth() === 7 && now.getDate() === 3) {
+        response.write('YES');
+    } else {
+        response.write('NO');
+    }
+});
+router.get('/wedding-day', function(request, response) {
+    const now = new Date();
+    if (now.getMonth() === 8 && now.getDate() === 16) {
+        response.write('YES');
+    } else {
+        response.write('NO');
+    }
+});
+
 const server = http.createServer();
 
 server.on('request', (request, response) => {
